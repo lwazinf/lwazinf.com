662aa28abac087031d5abcd999170de0f34e59aa
Lwazi Ndlovu <42185877+lwazin@users.noreply.github.com>
2022-07-26 00:56:46 +0200

    design :: menu done. missing call-to-action

--- pages/BonganiArc_.tsx
+++ pages/BonganiArc_.tsx
@@ -12,7 +12,9 @@ const BonganiArc_ = ({}: BonganiArc_Props) => {
       <div
         className={`flex flex-col items-center justify-start w-full h-screen relative`}
       >
-        <div className={`absolute top-0 w-full h-screen overflow-hidden z-[-1]`}>
+        <div
+          className={`absolute top-0 w-full h-screen overflow-hidden z-[-1]`}
+        >
           <img
             src={`https://images.pexels.com/photos/7061672/pexels-photo-7061672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`}
             className={`w-full object-cover`}
@@ -22,11 +24,38 @@ const BonganiArc_ = ({}: BonganiArc_Props) => {
           className={`absolute top-0 w-full h-screen flex flex-col items-start justify-start overflow-hidden topFade`}
         />
         {/* 👇️👇️👇️ Services */}
-        <div className={`h-[90px] w-full bg-black mb-[50px] z-[1] flex flex-col justify-start items-start pl-[30px]`}>
-          <img src={`/assets/images/Logo0.png`} className={`w-[100px] object-cover mb-[-20px]`}/>
-          <p className={`text-[15px] w-[100px] min-h-[1px] text-center font-black text-white`}>Bongani</p>
-          <p className={`text-[10px] w-[100px] min-h-[1px] relative left-[20px] text-center top-3 font-black text-yellow-400`}>ARC</p>
+        <div
+          className={`h-[90px] w-full bg-black z-[1] flex flex-row justify-start items-start pl-[30px] pr-[130px]`}
+        >
+          <div
+            className={`h-[90px] w-[100px] flex flex-col justify-start items-start`}
+          >
+            <img
+              src={`/assets/images/Logo0.png`}
+              className={`w-[100px] object-cover mb-[-20px]`}
+            />
+            <p
+              className={`text-[15px] w-[100px] min-h-[1px] text-center font-black text-white`}
+            >
+              Bongani
+            </p>
+            <p
+              className={`text-[10px] w-[100px] min-h-[1px] relative left-[20px] text-center top-3 font-black text-yellow-400`}
+            >
+              ARC
+            </p>
+          </div>
+          <div className={`flex flex-row w-full h-full-400 mt-[50px] justify-center items-center`}>
+            <p className={`text-[20px] text-yellow-400 transition duration-500 hover:text-white cursor-pointer font-black mx-3`}>HOME</p>
+            <p className={`text-[20px] text-white transition duration-500 hover:text-yellow-200 cursor-pointer font-black mx-3`}>CONTACT</p>
+            <p className={`text-[20px] text-white transition duration-500 hover:text-yellow-200 cursor-pointer font-black mx-3`}>B'ARC</p>
+            <p className={`text-[20px] text-white transition duration-500 hover:text-yellow-200 cursor-pointer font-black mx-3`}>SERVICES</p>
+          </div>
+          
         </div>
+        <div
+          className={`flex flex-row items-center justify-center bg-yellow-400/30 w-[650px] mb-[90px] min-h-[1px] relative`}
+        ></div>
         <div
           className={`flex flex-row items-center justify-center w-full min-h-[20px] relative mb-[10px] pointer-events-none`}
         >
@@ -38,9 +67,7 @@ const BonganiArc_ = ({}: BonganiArc_Props) => {
           <div className={`w-[5px] h-[5px] rounded-[50%] bg-yellow-400 m-1`} />
           <p className={`text-[16px] font-normal text-white`}>Architecture</p>
         </div>
-        <div
-          className={`flex flex-row items-center justify-center bg-yellow-400/30 w-[650px] min-h-[1px] relative`}
-        ></div>
+        
         {/* 👇️👇️👇️ Main Header */}
         <div
           className={`flex flex-row items-center justify-center w-full min-h-[20px] relative pointer-events-none`}
@@ -53,7 +80,7 @@ const BonganiArc_ = ({}: BonganiArc_Props) => {
           </p>
         </div>
         <div
-          className={`flex flex-row items-center justify-center bg-yellow-400/30 w-[450px] min-h-[1px] relative pointer-events-none`}
+          className={`flex flex-row items-center justify-center bg-yellow-400/30 w-[250px] min-h-[1px] relative pointer-events-none`}
         ></div>
         {/* 👇️👇️👇️ Hero Copy */}
         <div
@@ -111,7 +138,11 @@ const BonganiArc_ = ({}: BonganiArc_Props) => {
                   onClick={() => {}}
                 />
               </div>
-              <p className={`text-white text-[12px] font-light pointer-events-none`}>5.0 Rating</p>
+              <p
+                className={`text-white text-[12px] font-light pointer-events-none`}
+              >
+                5.0 Rating
+              </p>
             </div>
           </div>
           <div
@@ -155,7 +186,11 @@ const BonganiArc_ = ({}: BonganiArc_Props) => {
                   onClick={() => {}}
                 />
               </div>
-              <p className={`text-white text-[12px] font-light pointer-events-none`}>5.0 Rating</p>
+              <p
+                className={`text-white text-[12px] font-light pointer-events-none`}
+              >
+                5.0 Rating
+              </p>
             </div>
           </div>
         </div>
